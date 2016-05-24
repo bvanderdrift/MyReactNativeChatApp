ce0f9ae2e812e31d0a5be554bfdf134d93ebb854
Beer van der Drift <beerdrift@gmail.com>
2016-05-24 11:59:38 +0200

    Gave the input view a TextInput, label and some styling.

--- source/LoginPage.js
+++ source/LoginPage.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import {
-  Text
+	View,
+	Text,
+	TextInput,
+	StyleSheet
 } from 'react-native';
+
+var styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center'
+	},
+	usernameInputContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingLeft: 30,
+		paddingRight: 30
+	},
+	usernameLabel: {
+		flex: 1
+	},
+	usernameInput: {
+		flex: 4
+	}
+})
+
+var texts = {
+	loginNameInputLabel: "Username: ",
+	loginNameInputPlaceHolder: "Please provide a username"
+};
 
 class LoginPage extends Component {
   render() {
     return (
-      <Text>This is the login page!</Text>
+    	<View style={styles.container}>
+    		<View style={styles.usernameInputContainer}>
+	    		<Text style={styles.usernameLabel}>{texts.loginNameInputLabel}</Text>
+	    		<TextInput  style={styles.usernameInput} placeholder={texts.loginNameInputPlaceHolder} />
+    		</View>
+    	</View>
     );
   }
 }
